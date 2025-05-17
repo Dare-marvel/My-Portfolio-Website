9790ca1a2eff6043867ebfc8efd650bc90140a66
Adwait S Purao <adwait.purao@spit.ac.in>
2025-05-17 18:40:28 +0530

    modified the wave svg

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,6 +5,7 @@ import { loadFull } from "tsparticles";
 import { tsParticles } from "@tsparticles/engine";
 import starryNightConfig from './starryNightConfig';
 import rudder from '../assets/rudder.png'
+import waveSrc from "../assets/wave.svg";
 
 const Hero = () => {
   const compassRef = useRef<HTMLDivElement>(null);
@@ -78,15 +79,9 @@ const Hero = () => {
 
       {/* Wave animations */}
       <div className="absolute bottom-0 left-0 w-full z-20 opacity-40">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" preserveAspectRatio="none" className="w-full h-20">
-          <path fill="#0c4160" fillOpacity="0.8" d="M0,288L48,272C96,256,192,224,288,197.3C384,171,480,149,576,165.3C672,181,768,235,864,250.7C960,267,1056,245,1152,213.3C1248,181,1344,139,1392,117.3L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
-        </svg>
+        <img src={waveSrc} alt="Wave Animation" />
       </div>
-      <div className="absolute bottom-0 left-0 w-full z-10 opacity-40">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" preserveAspectRatio="none" className="w-full h-24">
-          <path fill="#1a6ca5" fillOpacity="0.5" d="M0,192L60,208C120,224,240,256,360,245.3C480,235,600,181,720,181.3C840,181,960,235,1080,234.7C1200,235,1320,181,1380,154.7L1440,128L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
-        </svg>
-      </div>
+      
     </section>
   );
 };
